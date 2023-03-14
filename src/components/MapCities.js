9bf56d123abb33215374797cc9d8e21be8c2ec10
Edwardo <edwardotroncoso@gmail.com>
2023-03-14 16:25:36 -0400

    Created an array of objects, passed the array using props to the MapCities component, and then used a handler function to console.log the input changes

---
+++ src/components/MapCities.js
@@ -0,0 +1,11 @@
+const MapCities = (props) => {
+    return (
+        props.cities.map(cities =>
+              <ul key={cities.key}>
+                <li>{cities.englishName}, {cities.administrativeArea.id}, {cities.country.id}</li>
+              </ul>
+        )
+    )
+}
+
+export default MapCities;
